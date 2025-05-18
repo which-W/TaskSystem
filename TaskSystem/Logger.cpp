8a6d7359502f3eca65262449a32961222c363c53
which-W <wengzu.love@gmail.com>
2025-05-18 17:51:41 +0800

    add num_threads to support concurrent

--- TaskSystem/Logger.cpp
+++ TaskSystem/Logger.cpp
@@ -1,5 +1,63 @@
 #include "Logger.h"
 namespace fs = std::filesystem;
+
+Logger::Logger() : _exit_flag(false),
+_console_output(false),
+_max_file_size(100 * 1024),
+_base_filename("Task.log"),
+_num_threads(3) {
+	open_log_file();
+	if (!_log_file) {
+		throw std::runtime_error("log file open is error");
+	}
+	for (int i = 1; i < _num_threads; i++) {
+		_work_threads.emplace_back(std::thread([this]() {
+			std::string msg;
+			while (_log_queue.pop(msg)) {
+				check_and_rotate_log_file();
+				_log_file << msg << std::endl;
+				if (_console_output) {
+					std::cout << msg << std::endl;
+				}
+			}
+			}));
+	};
+}
+
+Logger::Logger(const std::string& filename, bool console_output = false, size_t max_file_size_kb = 100, int num_threads = 3) :_exit_flag(false)
+, _console_output(console_output), _max_file_size(max_file_size_kb * 1024), _base_filename(filename), _num_threads(num_threads) {
+	open_log_file();
+	if (!_log_file) {
+		throw std::runtime_error("log file open is error");
+	}
+
+	for (int i = 1; i < _num_threads; i++) {
+		_work_threads.emplace_back(std::thread([this]() {
+			std::string msg;
+			while (_log_queue.pop(msg)) {
+				check_and_rotate_log_file();
+				_log_file << msg << std::endl;
+				if (_console_output) {
+					std::cout << msg << std::endl;
+				}
+			}
+			}));
+	};
+}
+
+Logger::~Logger() {
+	_exit_flag = true;
+	_log_queue.shutdown();
+	for (auto& thread : _work_threads) {
+		if (thread.joinable()) {
+			thread.join();
+		}
+	}
+	if (_log_file.is_open()) {
+		_log_file.close();
+	}
+}
+
 void Logger::open_log_file() {
 	if (_log_file.is_open()) {
 		_log_file.close();
@@ -19,7 +77,6 @@ size_t Logger::get_file_size() {
 	}
 	return (size_t)fs::file_size(_base_filename);
 }
-
 
 void Logger::rotate_log_file() {
 	_log_file.close();
@@ -43,3 +100,59 @@ std::string Logger::get_time() {
 	return std::string(buffer);
 }
 
+template<typename ...Args>
+void Logger::log(LogLevel Loglevel, const std::string& format, Args&& ...args) {
+	std::lock_guard<std::mutex> lock(mtx);
+	std::string loglevel_str;
+	switch (Loglevel) {
+	case LogLevel::INFO: loglevel_str = "[INFO] "; break;
+	case LogLevel::DEBUG: loglevel_str = "[DEBUG] "; break;
+	case LogLevel::ERROR: loglevel_str = "[ERROR] "; break;
+	}
+	_log_queue.push(loglevel_str + formatMessage(format, std::forward<Args>(args)...));
+}
+
+template<typename... Args>
+void Logger::console_log(LogLevel level, const std::string& format, Args&&... args) {
+	std::lock_guard<std::mutex> lock(mtx);
+	std::string loglevel_str;
+	switch (level) {
+	case INFO: loglevel_str = "[INFO] "; break;
+	case DEBUG: loglevel_str = "[DEBUG] "; break;
+	case WARN: loglevel_str = "[WARN] "; break;
+	case ERROR: loglevel_str = "[ERROR] "; break;
+	}
+	std::cout << (loglevel_str + formatMessage(format, std::forward<Args>(args)...)) << std::endl;
+}
+
+template<typename... Args>
+std::string Logger::formatMessage(const std::string& format, Args&&... args) {
+	std::vector<std::string> arg_strings = { to_string_helper(std::forward<Args>(args))... };
+	std::ostringstream oss;
+	size_t arg_index = 0;
+	size_t pos = 0;
+	size_t placeholder = format.find("{}", pos);
+
+	while (placeholder != std::string::npos) {
+		oss << format.substr(pos, placeholder - pos);
+		if (arg_index < arg_strings.size()) {
+			oss << arg_strings[arg_index++];
+		}
+		else {
+			// 没有足够的参数，保留 "{}"
+			oss << "{}";
+		}
+		pos = placeholder + 2; // 跳过 "{}"
+		placeholder = format.find("{}", pos);
+	}
+
+	// 添加剩余的字符串
+	oss << format.substr(pos);
+
+	// 如果还有剩余的参数，按原方式拼接
+	while (arg_index < arg_strings.size()) {
+		oss << arg_strings[arg_index++];
+	}
+
+	return "[" + get_time() + "] " + oss.str();
+}
--- TaskSystem/Logger.h
+++ TaskSystem/Logger.h
@@ -11,86 +11,18 @@ public:
 		return single;
 	}
 	// 新增：默认构造函数用于单例
-	Logger() : _exit_flag(false),
-		_console_output(false),
-		_max_file_size(100 * 1024),
-		_base_filename("Task.log") {
-		open_log_file();
-		if (!_log_file) {
-			throw std::runtime_error("log file open is error");
-		}
-
-		_work_threads = std::thread([this]() {
-			std::string msg;
-			while (_log_queue.pop(msg)) {
-				check_and_rotate_log_file();
-				_log_file << msg << std::endl;
-				if (_console_output) {
-					std::cout << msg << std::endl;
-				}
-			}
-			});
-	}
+	Logger();
 	Logger(const Logger&& other) = delete;
 	Logger& operator = (const Logger&& other) = delete;
 	Logger(const Logger& other) = delete;
 	Logger& operator = (const Logger& other) = delete;
-
-	Logger(const std::string& filename, bool console_output = false, size_t max_file_size_kb = 100) :_exit_flag(false)
-		, _console_output(console_output), _max_file_size(max_file_size_kb * 1024), _base_filename(filename) {
-		open_log_file();
-		if (!_log_file) {
-			throw std::runtime_error("log file open is error");
-		}
-
-		_work_threads = std::thread([this]() {
-			std::string msg;
-			while (_log_queue.pop(msg)) {
-				check_and_rotate_log_file();
-				_log_file << msg << std::endl;
-				if (_console_output) {
-					std::cout << msg << std::endl;
-				}
-			}
-
-			});
-	}
-	~Logger() {
-		_exit_flag = true;
-		_log_queue.shutdown();
-		if (_work_threads.joinable()) {
-			_work_threads.join();
-		}
-		if (_log_file.is_open()) {
-			_log_file.close();
-		}
-	}
-
+	Logger(const std::string& filename, bool console_output = false, size_t max_file_size_kb = 100, int num_threads = 3);
+	~Logger();
 
 	template<typename ...Args>
-	void log(LogLevel Loglevel, const std::string& format, Args&& ...args) {
-		std::lock_guard<std::mutex> lock(mtx);
-		std::string loglevel_str;
-		switch (Loglevel) {
-		case LogLevel::INFO: loglevel_str = "[INFO] "; break;
-		case LogLevel::DEBUG: loglevel_str = "[DEBUG] "; break;
-		case LogLevel::ERROR: loglevel_str = "[ERROR] "; break;
-		}
-		_log_queue.push(loglevel_str + formatMessage(format, std::forward<Args>(args)...));
-	}
+	void log(LogLevel Loglevel, const std::string& format, Args&& ...args);
 	template<typename... Args>
-	void console_log(LogLevel level, const std::string& format, Args&&... args) {
-		std::lock_guard<std::mutex> lock(mtx);
-		std::string loglevel_str;
-		switch (level) {
-		case INFO: loglevel_str = "[INFO] "; break;
-		case DEBUG: loglevel_str = "[DEBUG] "; break;
-		case WARN: loglevel_str = "[WARN] "; break;
-		case ERROR: loglevel_str = "[ERROR] "; break;
-		}
-		std::cout << (loglevel_str + formatMessage(format, std::forward<Args>(args)...)) << std::endl;
-	}
-
+	void console_log(LogLevel level, const std::string& format, Args&&... args);
 
 
 private:
@@ -102,45 +34,15 @@ private:
 
 	void rotate_log_file();
 
-
 	//时间戳
 	std::string get_time();
 	// 使用模板折叠格式化日志消息，支持 "{}" 占位符
 	template<typename... Args>
-	std::string formatMessage(const std::string& format, Args&&... args) {
-		std::vector<std::string> arg_strings = { to_string_helper(std::forward<Args>(args))... };
-		std::ostringstream oss;
-		size_t arg_index = 0;
-		size_t pos = 0;
-		size_t placeholder = format.find("{}", pos);
-
-		while (placeholder != std::string::npos) {
-			oss << format.substr(pos, placeholder - pos);
-			if (arg_index < arg_strings.size()) {
-				oss << arg_strings[arg_index++];
-			}
-			else {
-				// 没有足够的参数，保留 "{}"
-				oss << "{}";
-			}
-			pos = placeholder + 2; // 跳过 "{}"
-			placeholder = format.find("{}", pos);
-		}
-
-		// 添加剩余的字符串
-		oss << format.substr(pos);
-
-		// 如果还有剩余的参数，按原方式拼接
-		while (arg_index < arg_strings.size()) {
-			oss << arg_strings[arg_index++];
-		}
-
-		return "[" + get_time() + "] " + oss.str();
-	}
+	std::string formatMessage(const std::string& format, Args&&... args);
 
 
 
-	std::thread _work_threads;
+	std::vector<std::thread> _work_threads;;
 	std::ofstream _log_file;
 	std::atomic<bool> _exit_flag;
 	LogQueue _log_queue;
@@ -149,5 +51,6 @@ private:
 	std::string _base_filename;
 	int _current_log_index = 0;
 	std::mutex mtx; // 互斥锁，保护日志输出
+	int _num_threads;
 };
 
